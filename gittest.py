25c4f8ba1d72c3cad3c8313a78b8a86a942bef11
cafe8601 <inhiyu@gmail.com>
2022-01-10 20:41:30 +0900

    first commit

---
+++ gittest.py
@@ -0,0 +1,27 @@
+from threading import Thread
+import threading
+
+class Cultery:
+    # knives = attrib(default = 0)
+    # forks = attrib(default = 0)
+    
+    def __init__(self, knives, forks):
+        self.knives = knives
+        self.forks = forks
+        self.lock = threading.Lock()
+    
+    def give(self, to: 'Cultery', knives = 0, forks = 0):
+        self.change(-knives, -forks)
+        to.change(knives, forks)
+        
+    def change(self, knives, forks):
+        # with self.lock:
+        self.lock.acquire()
+        self.knives += knives
+        self.forks += forks
+        self.lock.release()
+        
+if __name__ == "__main__":
+    a = Cultery(5, 8)
+    a.change(2, 3)
+    print(f"{a.knives} , {a.forks}") 
